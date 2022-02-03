6965177c4b72e0ec8d09891d34a0deac32f32233
Tran Ngoc Phuoc <phuoc04012000@gmail.com>
2022-02-03 21:25:11 +0700

    React router active link using NavLink

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,16 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
 
 export const Navbar = () => {
-    return <nav>
-    <Link to='/'>Home</Link> 
-    <Link to='/about'>About</Link>
-    </nav>;
+    const navLinkStyles = ({isActive}) => {
+        return {
+            fontWeight: isActive ? 'bold' : 'normal', textDecoration: isActive ? 'none' : 'underLine',
+
+        }
+    }
+
+    return (<nav>
+        <NavLink style={navLinkStyles} to='/'>Home</NavLink>
+        <NavLink style={navLinkStyles} to='/about'>About</NavLink>
+    </nav>);
 };
