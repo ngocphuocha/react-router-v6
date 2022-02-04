fd65ed46e00005db709e61006cbceccda3cef41a
Tran Ngoc Phuoc <phuoc04012000@gmail.com>
2022-02-04 18:43:49 +0700

    search params using useSearchParams hook

--- src/App.js
+++ src/App.js
@@ -1,35 +1,39 @@
-import {Routes, Route} from "react-router-dom";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
-import About from "./components/About";
-import {Navbar} from "./components/Navbar";
+import { Navbar } from "./components/Navbar";
 import OrderSummary from "./components/OrderSummary";
 import Product from "./components/Product";
 import FeaturedProducts from "./components/FeaturedProducts";
 import NewProducts from "./components/NewProducts";
 import NoMatch from "./components/NoMatch";
-import {Users} from "./components/Users";
-import {UserDetail} from "./components/UserDetail";
-import {Admins} from "./components/Admins";
+import { Users } from "./components/Users";
+import { UserDetail } from "./components/UserDetail";
+import { Admins } from "./components/Admins";
+const LazyAbout = React.lazy(() => import("./components/About"))
 
 function App() {
     return (
         <>
-            <Navbar/>
+            <Navbar />
 
             <Routes>
-                <Route path="/" element={<Home/>}/>
-                <Route path="about" element={<About/>}/>
-                <Route path="order-summary" element={<OrderSummary/>}/>
-                <Route path="product" element={<Product/>}>
-                    <Route index element={<FeaturedProducts/>}/>
-                    <Route path="featured" element={<FeaturedProducts/>}/>
-                    <Route path="new" element={<NewProducts/>}/>
+                <Route path="/" element={<Home />} />
+                <Route path="about" element={
+                    <React.Suspense fallback={<>Loading...</>}>
+                        <LazyAbout />
+                    </React.Suspense>} />
+                <Route path="order-summary" element={<OrderSummary />} />
+                <Route path="product" element={<Product />}>
+                    <Route index element={<FeaturedProducts />} />
+                    <Route path="featured" element={<FeaturedProducts />} />
+                    <Route path="new" element={<NewProducts />} />
                 </Route>
-                <Route path="users" element={<Users/>}>
-                    <Route path=":userId" element={<UserDetail/>}/>
-                    <Route path="admins" element={<Admins/>}/>
+                <Route path="users" element={<Users />}>
+                    <Route path=":userId" element={<UserDetail />} />
+                    <Route path="admins" element={<Admins />} />
                 </Route>
-                <Route path="*" element={<NoMatch/>}/>
+                <Route path="*" element={<NoMatch />} />
             </Routes>
         </>
     )
--- src/components/Users.js
+++ src/components/Users.js
@@ -1,7 +1,41 @@
-import React from "react";
-import {Outlet} from "react-router-dom";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Outlet, useSearchParams } from "react-router-dom";
 
 export const Users = () => {
+    const [searchParams, setSearchParams] = useSearchParams()
+
+    const showActiveUsers = searchParams.get('filter') === 'active'
+    let userId = searchParams.get('userId');
+    const [user, setUser] = useState('')
+    const [error, setError] = useState('')
+
+    const changeUserIdParam = (e) => {
+        setSearchParams({ userId: e.target.value })
+    }
+
+    const handleSearchUser = async () => {
+        try {
+            if (userId > 0) {
+                const response = await axios.get(`https://jsonplaceholder.typicode.com/users/${userId}`)
+                console.log(response);
+                setUser(response.data.name)
+                setError('')
+            } else {
+                setUser('')
+                setError('')
+            }
+        } catch (error) {
+            if (error.response.status === 404) {
+                setError('User Not found')
+            }
+            console.log(error);
+        }
+    }
+
+    useEffect(() => {
+        handleSearchUser()
+    }, [userId])
 
     return (
         <div>
@@ -9,7 +43,25 @@ export const Users = () => {
             <h1>User 2</h1>
             <h1>User 3</h1>
 
-            <Outlet/>
+            <Outlet />
+            {/* <div>{user}</div> */}
+            <div>
+                <input onKeyUp={changeUserIdParam} type="text" placeholder="Enter user id" />
+                <div className="result">
+                    {user}
+                    {error}
+                </div>
+                <button onClick={handleSearchUser}>Search</button>
+                <button onClick={() => setSearchParams({ filter: 'active' })}>
+                    Active Users
+                </button>
+                <button onClick={() => setSearchParams({})}>Reset Filter</button>
+            </div>
+            {showActiveUsers ? (
+                <h2>Showing active users</h2>
+            ) : (
+                <h2>Showing all users</h2>
+            )}
         </div>
     );
 };
